59c38daf5a230f9127dfb9a88b68d97cded6e4e8
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-06 11:49:46 -0600

    init commit; psql basics

---
+++ playground/psql-basics.sql
@@ -0,0 +1,14 @@
+--
+-- psql commands
+--
+
+-- for help: \?
+-- list databases: \l
+-- connect to a new database: \c <database_name>
+
+
+--
+-- sql commands
+--
+
+-- create database: CREATE DATABASE <database_name
