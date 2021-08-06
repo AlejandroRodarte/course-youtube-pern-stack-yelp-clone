c60bdf6f046d638fe240edad1d0560683bfe98d3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-06 11:56:11 -0600

    Creating tables

--- playground/psql-basics.sql
+++ playground/psql-basics.sql
@@ -5,7 +5,8 @@
 -- for help: \?
 -- list databases: \l
 -- connect to a new database: \c <database_name>
-
+-- listing tables in current database: \d
+-- watch table specifics: \d <table_name>
 
 --
 -- sql commands
