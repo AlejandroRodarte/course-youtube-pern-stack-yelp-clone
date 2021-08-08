69f4ef539a3a385896d41aef241c79e977d06db5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-07 22:20:55 -0600

    Get one restaurant route

--- server/src/routes/v1/restaurants/handlers/get-restaurant.js
+++ server/src/routes/v1/restaurants/handlers/get-restaurant.js
@@ -1,15 +1,60 @@
-const getRestaurant = (req, res) => {
-    res.status(200).send({
-        status: 'OK',
-        data: {
-            restaurant: {
-                id: +req.params.id,
-                name: 'McDonalds',
-                location: 'Seattle',
-                priceRange: 3
-            }
+const getRestaurant = async (req, res) => {
+
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        return res
+                .status(400)
+                .send({
+                    status: 'Error',
+                    data: {
+                        message: 'Please provide a valid id.'
+                    }
+                });
+    }
+
+    try {
+
+        const query = 
+            await req
+                    .app
+                    .get('db')
+                    .query(
+                        'SELECT * FROM restaurants WHERE id = $1',
+                        [+id]
+                    );
+
+        const [restaurant] = query.rows;
+
+        if (!restaurant) {
+            return res
+                    .status(404)
+                    .send({
+                        status: 'Error',
+                        data: {
+                            message: 'No restaurant was found.'
+                        }
+                    });
         }
-    });
+
+        res.status(200).send({
+            status: 'OK',
+            data: {
+                restaurant
+            }
+        });
+
+    } catch (e) {
+        res
+            .status(400)
+            .send({
+                status: 'Error',
+                data: {
+                    message: 'There was an error performing the query.'
+                }
+            });
+    }
+
 };
 
 module.exports = getRestaurant;
