586fa12fac45b5e8ff6ff7d6e74f39b7791c0e17
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-08 16:04:29 -0600

    Create a restaurant

--- server/src/routes/v1/restaurants/handlers/add-restaurant.js
+++ server/src/routes/v1/restaurants/handlers/add-restaurant.js
@@ -1,10 +1,41 @@
-const addRestaurant = (req, res) => {
-    res.status(201).send({
-        status: 'OK',
-        data: {
-            restaurant: req.body
-        }
-    });
+const addRestaurant = async (req, res) => {
+
+    const { name, location, priceRange } = req.body.data.restaurant;
+
+    try {
+        
+        const query = 
+            await req
+                    .app
+                    .get('db')
+                    .query(
+                        'INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) RETURNING *',
+                        [name, location, priceRange]
+                    );
+
+        const [newRestaurant] = query.rows;
+
+        res
+            .status(201)
+            .send({
+                status: 'OK',
+                data: {
+                    message: 'A new restaurant has been created.',
+                    restaurant: newRestaurant
+                }
+            });
+
+    } catch (e) {
+        res
+            .status(400)
+            .send({
+                status: 'Error',
+                data: {
+                    message: 'There was an error performing the insert.'
+                }
+            });
+    }
+
 };
 
 module.exports = addRestaurant;
