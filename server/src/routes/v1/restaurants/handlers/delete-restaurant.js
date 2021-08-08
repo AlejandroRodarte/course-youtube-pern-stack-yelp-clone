6432043907d3514610ab151c99b53e54ac69f98a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-08 16:20:13 -0600

    Delete a restaurant

--- server/src/routes/v1/restaurants/handlers/delete-restaurant.js
+++ server/src/routes/v1/restaurants/handlers/delete-restaurant.js
@@ -1,10 +1,49 @@
-const deleteRestaurant = (req, res) => {
-    res.status(200).send({
-        status: 'OK',
-        data: {
-            id: req.params.id
-        }
-    });
+const deleteRestaurant = async (req, res) => {
+
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        return res
+                .status(400)
+                .send({
+                    status: 'Error',
+                    data: {
+                        message: 'Please provide a valid id.'
+                    }
+                });
+    }
+
+    try {
+        
+        await req
+                .app
+                .get('db')
+                .query(
+                    'DELETE FROM restaurants WHERE id = $1',
+                    [id]
+                );
+
+        res
+            .status(204)
+            .send({
+                status: 'OK',
+                data: {
+                    message: 'The restaurant has been deleted.',
+                    id
+                }
+            });
+
+    } catch (e) {
+        res
+            .status(400)
+            .send({
+                status: 'Error',
+                data: {
+                    message: 'There was an error performing the delete.'
+                }
+            });
+    }
+
 };
 
 module.exports = deleteRestaurant;
