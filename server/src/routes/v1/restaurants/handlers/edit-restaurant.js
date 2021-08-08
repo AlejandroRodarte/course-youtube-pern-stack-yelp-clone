c5c53f4b034148234caad602d7af4b7ebcd52b3b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-08 16:15:43 -0600

    Update a restaurant

--- server/src/routes/v1/restaurants/handlers/edit-restaurant.js
+++ server/src/routes/v1/restaurants/handlers/edit-restaurant.js
@@ -1,11 +1,54 @@
-const editRestaurant = (req, res) => {
-    res.status(200).send({
-        status: 'OK',
-        data: {
-            id: req.params.id,
-            newRestaurant: req.body
-        }
-    });
+const editRestaurant = async (req, res) => {
+
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        return res
+                .status(400)
+                .send({
+                    status: 'Error',
+                    data: {
+                        message: 'Please provide a valid id.'
+                    }
+                });
+    }
+
+    const { name, location, priceRange } = req.body.data.restaurant;
+
+    try {
+        
+        const query = 
+            await req
+                    .app
+                    .get('db')
+                    .query(
+                        'UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *',
+                        [name, location, priceRange, id]
+                    );
+
+        const [updatedRestaurant] = query.rows;
+
+        res
+            .status(201)
+            .send({
+                status: 'OK',
+                data: {
+                    message: 'The restaurant has been updated.',
+                    restaurant: updatedRestaurant
+                }
+            });
+
+    } catch (e) {
+        res
+            .status(400)
+            .send({
+                status: 'Error',
+                data: {
+                    message: 'There was an error performing the update.'
+                }
+            });
+    }
+
 };
 
 module.exports = editRestaurant;
--- server/src/routes/v1/restaurants/index.js
+++ server/src/routes/v1/restaurants/index.js
@@ -9,7 +9,7 @@ const router = Router();
 router.get('/', handlers.getRestaurants);
 router.get('/:id', handlers.getRestaurant);
 router.post('/', validateObject(specs.restaurant, 'data.restaurant'), handlers.addRestaurant);
-router.put('/:id', handlers.editRestaurant);
+router.put('/:id', validateObject(specs.restaurant, 'data.restaurant'), handlers.editRestaurant);
 router.delete('/:id', handlers.deleteRestaurant);
 
 module.exports = router;
