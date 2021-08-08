9fb1a598bc0a99a7a3ac5b95a018b744a0dd973b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-08 17:12:31 -0600

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+const App = () => (
+  <div className='container bg-light shadow'>
+    <p className='display-2'>I'm the container</p>
+    <i class="fas fa-user"></i>
+  </div>
+);
+
+export default App;
