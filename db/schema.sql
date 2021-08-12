0e27e28c4e074c6bfa6254dbf61ae4d9a787a686
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-08-11 18:08:03 -0600

    Reviews Postgres Table

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS restaurants (
     location VARCHAR(50) NOT NULL,
     price_range INT NOT NULL check(price_range >= 1 AND price_range <= 5)
 );
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    rating INT NOT NULL check(rating >= 1 AND rating <= 5),
+    review TEXT NOT NULL,
+    restaurant_id BIGINT NOT NULL,
+    CONSTRAINT fk_restaurant
+        FOREIGN KEY(restaurant_id)
+            REFERENCES restaurants(id)
+);
---
+++ playground/insert/reviews.sql
@@ -0,0 +1,2 @@
+INSERT INTO reviews (name, rating, review, restaurant_id)
+VALUES ('Beth', 3, 'Average at best.', 21);
